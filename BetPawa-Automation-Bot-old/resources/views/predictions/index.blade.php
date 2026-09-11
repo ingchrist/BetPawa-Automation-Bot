7dd1d853377af744bf7cd4e40748a8a0491ec630
ingchrist <cdjinguet@gmail.com>
2026-09-11 18:32:03 +0100

    updating

--- BetPawa-Automation-Bot-old/resources/views/predictions/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container mx-auto px-4 py-8">
-    <h1 class="text-2xl font-bold mb-6">Predictions</h1>
-    <predictions-list></predictions-list>
-</div>
-@endsection 
